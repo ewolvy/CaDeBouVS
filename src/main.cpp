d779f87b45994c7cb2b99066870fc61574f07793
Juanjo Soriano <23655794+ewolvy@users.noreply.github.com>
2020-09-26 09:37:46 +0200

    Leer de todos los sensores de distancia

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,17 @@ void setup() {
   Serial.begin(115200);
   Wire.begin();
   setup_sensor(VLDIAGIZQ, TYPE_VL);
+  setup_sensor(VLFRONIZQ, TYPE_VL);
+  setup_sensor(VLDIAGDCHA, TYPE_VL);
+  setup_sensor(VLFRONDCHA, TYPE_VL);
 }
 
 void loop() {
   Serial.println(read_mm(VLDIAGIZQ));
+  Serial.println(read_mm(VLFRONIZQ));
+  Serial.println(read_mm(VLDIAGDCHA));
+  Serial.println(read_mm(VLFRONDCHA));
+  Serial.println();
 
   delay(500);
 }
