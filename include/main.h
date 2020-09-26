a3bb671643afeded824a174182b64e98d3749666
Juanjo Soriano <23655794+ewolvy@users.noreply.github.com>
2020-09-26 17:12:22 +0200

    Añadidas funciones para ver el voltaje de las baterías

---
+++ include/main.h
@@ -0,0 +1,10 @@
+#ifndef main_h
+
+#define main_h
+
+#include <Arduino.h>
+#include <Wire.h>
+#include "setup.h"
+#include "tft.h"
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,39 @@
-#include <Arduino.h>
-#include <Wire.h>
-#include "setup.h"
+#include "main.h"
 
 void setup() {
   Serial.begin(115200);
+  /*
+  Inicialización de sensores de distancia
+  
   Wire.begin();
   setup_sensor(VLDIAGIZQ, TYPE_VL);
   setup_sensor(VLFRONIZQ, TYPE_VL);
   setup_sensor(VLDIAGDCHA, TYPE_VL);
   setup_sensor(VLFRONDCHA, TYPE_VL);
+  */
+
+  /*
+  Inicialización de la pantalla
+  setup_tft();
+  */
 }
 
 void loop() {
+  /*
+  Pruebas de los sensores de distancia
+  
   Serial.println(read_mm(VLDIAGIZQ));
   Serial.println(read_mm(VLFRONIZQ));
   Serial.println(read_mm(VLDIAGDCHA));
   Serial.println(read_mm(VLFRONDCHA));
   Serial.println();
+  */
+
+  /*
+  Mostrar el voltaje en pantalla
+
+  show_voltage();
+  */
 
   delay(500);
 }
